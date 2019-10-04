18f02cfce856d7b1e183fd7012220655af434487
VladimirZhdanov <consthomespb@gmail.com>
2019-10-04 20:04:25 +0300

    Task 1 - Anagrams

--- javaEE/src/test/java/com/foxminded/anagrams/AnagramsTest.java
+++ javaEE/src/test/java/com/foxminded/anagrams/AnagramsTest.java
@@ -1,27 +1,28 @@
 package com.foxminded.anagrams;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * @author Vladimir Zhdanov (mailto:constHomeSpb@gmail.com)
  * @since 0.1
  */
 public class AnagramsTest {
-    private Anagrams anagrams;
-    private static final String ONLY_LETTERS = "abcd efgh";
-    private static final String REVERSED_LETTERS = "dcba hgfe";
-    private static final String ONLY_NUMBERS = "32432 324234";
-    private static final String ONLY_SYMBOLS = "№@#$%^&*()/\"|\\;:";
-    private static final String LETTERS_AND_SYMBOLS = "abcd №@#$%^&*()/\"|\\;:";
-    private static final String REVERSED_LETTERS_AND_SYMBOLS = "dcba №@#$%^&*()/\"|\\;:";
-    private static final String LETTERS_AND_NUMBERS = "abcd 324234";
-    private static final String REVERSED_LETTERS_AND_NUMBERS = "dcba 324234";
-    private static final String LETTERS_NUMBERS_SYMBOLS = " c1onstHomeSPB@gmail.com";
-    private static final String REVERSED_LETTERS_NUMBERS_SYMBOLS = " m1ocliamgBPSe@moHts.noc";
-    private static final String WHITESPACE = " ";
+    public Anagrams anagrams;
+    public static final String ONLY_LETTERS = "abcd efgh";
+    public static final String REVERSED_LETTERS = "dcba hgfe";
+    public static final String ONLY_NUMBERS = "32432 324234";
+    public static final String ONLY_SYMBOLS = "№@#$%^&*()/\"|\\;:";
+    public static final String LETTERS_AND_SYMBOLS = "abcd №@#$%^&*()/\"|\\;:";
+    public static final String REVERSED_LETTERS_AND_SYMBOLS = "dcba №@#$%^&*()/\"|\\;:";
+    public static final String LETTERS_AND_NUMBERS = "abcd 324234";
+    public static final String REVERSED_LETTERS_AND_NUMBERS = "dcba 324234";
+    public static final String LETTERS_NUMBERS_SYMBOLS = " c1onstHomeSPB@gmail.com";
+    public static final String REVERSED_LETTERS_NUMBERS_SYMBOLS = " m1ocliamgBPSe@moHts.noc";
+    public static final String WHITESPACE = " ";
 
     @BeforeEach
     void initEach() {
@@ -37,43 +38,50 @@ public class AnagramsTest {
 
     @Test
     void shouldDoNothingWhenApplyNumbersToTheMethod() {
-        assertEquals(ONLY_NUMBERS, anagrams.reverseString(ONLY_NUMBERS),
+        String actual = anagrams.reverseString(ONLY_NUMBERS);
+        assertEquals(ONLY_NUMBERS, actual,
                 "Reverse method should return the non-reversed numbers");
     }
 
     @Test
     void shouldDoNothingWhenApplySymbolsToTheMethod() {
-        assertEquals(ONLY_SYMBOLS, anagrams.reverseString(ONLY_SYMBOLS),
+        String actual = anagrams.reverseString(ONLY_SYMBOLS);
+        assertEquals(ONLY_SYMBOLS, actual,
                 "Reverse method should return the non-reversed symbols");
     }
 
     @Test
     void shouldReverseStringWhenApplyLettersToTheMethod() {
-        assertEquals(REVERSED_LETTERS, anagrams.reverseString(ONLY_LETTERS),
+        String actual = anagrams.reverseString(ONLY_LETTERS);
+        assertEquals(REVERSED_LETTERS, actual,
                 "Reverse method should return the reversed letters");
     }
 
     @Test
     void shouldReverseLettersWhenApplyStringToTheMethod1() {
-        assertEquals(REVERSED_LETTERS_AND_NUMBERS, anagrams.reverseString(LETTERS_AND_NUMBERS),
+        String actual = anagrams.reverseString(LETTERS_AND_NUMBERS);
+        assertEquals(REVERSED_LETTERS_AND_NUMBERS, actual,
                 "Reverse method should return the reversed letters only!");
     }
 
     @Test
     void shouldReverseLettersWhenApplyStringToTheMethod2() {
-        assertEquals(REVERSED_LETTERS_AND_SYMBOLS, anagrams.reverseString(LETTERS_AND_SYMBOLS),
+        String actual = anagrams.reverseString(LETTERS_AND_SYMBOLS);
+        assertEquals(REVERSED_LETTERS_AND_SYMBOLS, actual,
                 "Reverse method should return the reversed letters only!");
     }
 
     @Test
     void shouldReverseLettersWhenApplyStringToTheMethod3() {
-        assertEquals(REVERSED_LETTERS_NUMBERS_SYMBOLS, anagrams.reverseString(LETTERS_NUMBERS_SYMBOLS),
+        String actual = anagrams.reverseString(LETTERS_NUMBERS_SYMBOLS);
+        assertEquals(REVERSED_LETTERS_NUMBERS_SYMBOLS, actual,
                 "Reverse method should return the reversed letters only!");
     }
 
     @Test
     void shouldDeleteWhiteSpaceWhenApplyWhiteSpaceToTheMethod() {
-        assertEquals("", anagrams.reverseString(WHITESPACE),
+        String actual = anagrams.reverseString(WHITESPACE);
+        assertEquals("", actual,
                 "Reverse method should return empty string!");
     }
 }
