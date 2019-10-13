e6098987b588062abb06e92ba21be256bd6a9777
VladimirZhdanov <consthomespb@gmail.com>
2019-10-13 22:12:03 +0300

    Task 3 - Integer division

--- javaEE/src/test/java/com/foxminded/division/DivisionTest.java
+++ javaEE/src/test/java/com/foxminded/division/DivisionTest.java
@@ -27,35 +27,33 @@ class DivisionTest {
 
     @Test
     public void shouldReturnCorrectedColumnOfDivisionWhenGiveCorrectedParameters1() {
-        String expected = "_78945│4\n" +
-                " 4    │-----\n" +
-                " -    │19736\n" +
-                "_38\n" +
-                " 36\n" +
-                " --\n" +
-                " _29\n" +
-                "  28\n" +
-                "  --\n" +
-                "  _14\n" +
-                "   12\n" +
-                "   --\n" +
-                "   _25\n" +
-                "    24\n" +
-                "    --\n" +
-                "     1\n";
+        String expected = "_78945│4\n"
+                + " 4    │-----\n"
+                + " -    │19736\n"
+                + "_38\n"
+                + " 36\n"
+                + " --\n"
+                + " _29\n"
+                + "  28\n"
+                + "  --\n"
+                + "  _14\n"
+                + "   12\n"
+                + "   --\n"
+                + "   _25\n"
+                + "    24\n"
+                + "    --\n"
+                + "     1\n";
         String actual = division.makeDivision(78945, 4);
         assertEquals(expected, actual);
     }
 
     @Test
     public void shouldReturnCorrectedColumnOfDivisionWhenGiveCorrectedParameters2() {
-        String expected = "_456│11\n" +
-                " 44 │--\n" +
-                " -- │41\n" +
-                " _16\n" +
-                "  11\n" +
-                "  --\n" +
-                "   5\n";
+        String expected = "_456│11\n" + " 44 │--\n"
+                + " -- │41\n" + " _16\n"
+                + "  11\n"
+                + "  --\n"
+                + "   5\n";
         String actual = division.makeDivision(456, 11);
         assertEquals(expected, actual);
     }
